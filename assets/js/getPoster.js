10842fccd96cd805692e8e313319848337cfa221
Antoine Cornaz <antoine.cornaz01@gmail.com>
2025-05-29 18:23:02 +0200

    rename all files to lowercase and keep only a to z, 0 to 9 and '.'

--- assets/js/getPoster.js
+++ assets/js/getPoster.js
@@ -7,11 +7,12 @@
  */
 async function getPosterUrl(title, year) {
     const key          = 'f58a7502';  // your OMDb API key
-    const encodedTitle = encodeURIComponent(title);
-    const localUrl     = `./data/images/${year}/${title}.jpg`;
+    const encodedTitle = title.toLowerCase().replace(/[^a-z0-9\.]/g, '');
+    const localUrl     = `./data/images/${year}/${encodedTitle}.jpg`;
     const omdbApiUrl   = `https://www.omdbapi.com/?apikey=${key}&t=${encodedTitle}&y=${year}`;
     const placeholder  = './data/images/no-poster.jpeg'; // a generic fallback
 
+    console.log(encodedTitle);
     // 1) Check local file via HEAD
     try {
         const headResp = await fetch(localUrl, { method: 'HEAD' });
